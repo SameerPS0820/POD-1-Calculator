bd80e8ed4974ce492a9c7be85b8b74e323c1e04f
Sameer Raj <sameer.raj@publicissapient.com>
2025-02-25 13:37:16 +0530

    Implemented divide functionality

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,10 @@
 public class Calculator{
+
+    public static int divide(int number1, int number2){
+        return number1/number2;
+    }
     public static void main(String[] args) {
-        
+        int number1 = 10, number2 = 20;
+        divide(number1, number2);
     }
 }
